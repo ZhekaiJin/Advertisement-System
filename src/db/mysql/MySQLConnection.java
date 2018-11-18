660238a2fb512fe3c313f4765ca2e503d5968b84
ZhekaiJin <jinzhekai0705@163.com>
2018-11-17 19:10:15 -0500

    add mutiple endpoint & add second price bidding algo

--- src/db/mysql/MySQLConnection.java
+++ src/db/mysql/MySQLConnection.java
@@ -1,10 +1,12 @@
 package db.mysql;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DBConnection;
 import entity.AdItem;
+import entity.AdItem.AdItemBuilder;
 
 public class MySQLConnection implements DBConnection{
 
@@ -31,29 +33,102 @@ public class MySQLConnection implements DBConnection{
 		}
 	}
 
+	/**
+	 * Return all ads where their bid amount is > 0
+	 */
 	@Override
 	public List<AdItem> searchAdItems() {
-		// TODO Auto-generated method stub
-		return null;
+		if (conn == null) {
+			System.err.println("DB connection failed!");
+			return new ArrayList<>();
+		}
+		
+		List<AdItem> adItems = new ArrayList<>();
+		try {
+			// Get all ads candidates
+			String sql = "SELECT * FROM ad LEFT JOIN advertiser on ad.advertiser_id = advertiser.advertiser_id WHERE ad.bid > 0 AND advertiser.budget > 0";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			
+			ResultSet rs = stmt.executeQuery();
+			AdItemBuilder builder = new AdItemBuilder();
+			
+			while (rs.next()) {
+				builder.setAd_id(rs.getInt("ad_id"));
+				builder.setBid(rs.getFloat("bid"));
+				builder.setImage_url(rs.getString("image_url"));
+				builder.setAdvertiser_id(rs.getInt("advertiser_id"));
+				builder.setAd_score(rs.getFloat("ad_score"));
+				adItems.add(builder.build());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return adItems;
 	}
+
 
 	@Override
 	public float getBudget(int advertiser_id) {
-		// TODO Auto-generated method stub
-		return 0;
+		if (conn == null) {
+			System.err.println("DB connection failed!");
+		}
+		
+		float curBudget = -1;
+
+		try {	
+			String sql = "SELECT * FROM advertiser WHERE advertiser_id = (?)";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setDouble(1, advertiser_id);			
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				curBudget = rs.getFloat("budget");
+			}
+			System.out.println("curBudget" + curBudget);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return curBudget;
 	}
+
 
 	@Override
 	public void updateBudget(int advertiser_id, double budget) {
-		// TODO Auto-generated method stub
+		if (conn == null) {
+			System.err.println("DB connection failed!");
+		}
 		
+		try {	
+			String sql = "UPDATE advertiser SET budget=(?) WHERE advertiser_id=(?)";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setDouble(1, budget);
+			stmt.setInt(2, advertiser_id);
+			stmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("update budget done");
 	}
 
-	@Override
-	public void updateBid(int ad_id, double bid) {
-		// TODO Auto-generated method stub
-		
-	}
+
+	 @Override 
+		public void updateBid(int ad_id, double bid) {
+			if (conn == null) {
+				System.err.println("DB connection failed!");
+			}
+			
+			try {	
+				String sql = "UPDATE ad SET bid=(?) WHERE ad_id=(?)";
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				stmt.setDouble(1, bid);
+				stmt.setInt(2, ad_id);
+				System.out.println(stmt.toString());
+				stmt.execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			System.out.println("update bid done");
+		}
+
 
 	@Override
 	public long createAdvertiser(String advertiser_name, double budget) {
@@ -95,7 +170,38 @@ public class MySQLConnection implements DBConnection{
 	@Override
 	public long createAd(double bid, String image_url, int advertiser_id, double ad_score) {
 		// TODO Auto-generated method stub
-		return 0;
-	}
+			if (conn == null) {
+				System.err.println("DB connection failed!");
+			}
+			
+			//INSERT INTO ad (bid, image_url, advertiser_id, ad_score) VALUES ('2',"http://image.com", 3, 0.3);
+			try {	
+				String sql = "INSERT INTO ad (bid, image_url, advertiser_id, ad_score) VALUES ((?), (?), (?), (?))";
+				PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+				stmt.setDouble(1, bid);
+				stmt.setString(2, image_url);
+				stmt.setInt(3, advertiser_id);
+				stmt.setDouble(4, ad_score);
+				System.out.println(stmt.toString());
+				
+		        int affectedRows = stmt.executeUpdate();
+		        
+		        if (affectedRows == 0) {
+		            throw new SQLException("Creating ad failed, no rows affected.");
+		        }
 
+		        try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+		            if (generatedKeys.next()) {
+		            	return generatedKeys.getLong(1);
+		            }
+		            else {
+		                throw new SQLException("Creating ad failed, no ID obtained.");
+		            }
+		        }
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			System.out.println("insert ad done");
+			return -1;
+		}
 }
